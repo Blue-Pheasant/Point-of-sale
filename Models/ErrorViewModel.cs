db37c667372a79229217a6b0dcdd83b7cd24195e
Blue-Pheasant <BluePheasant@users.noreply.github.com>
2021-10-20 22:42:00 +0700

    template of asp.net mvc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace POS.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
