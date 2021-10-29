723a34ea3d9936a4ced7c5d58cafeeb41e2bdb1b
Blue-Pheasant <BluePheasant@users.noreply.github.com>
2021-10-29 13:12:27 +0700

    impleme config.php

--- config/config.php
+++ config/config.php
@@ -1,10 +1,51 @@
 <?php
-define('DEFAULT_CONTROLLER','HomeController');
-define('PROOT','/erp_sales/');
-define('SITE_TITLE','ERP Sales');
-define('DEFAULT_LAYOUT', 'default');
+    class database {
+        private $hostname = 'localhost';
+        private $username = 'root';
+        private $pass = '';
+        private $dbname = 'POS_SYSTEM';
 
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'erpsystem');
-define('DB_USER', 'root');
-define('DB_PASSWORD', '');
+        private $conn = NULL;
+        private $result = NULL;
+
+        //CONNECT TO DATABASE
+        public function connect($sql) {
+            $this->conn = new mysqli($this->hostname, $this->username, $this->pass, $this->dbname);
+            if(!$this->conn) {
+                echo "Connection failed";
+                exit();
+            }
+            else {
+                mysqli_set_charset($this->conn, 'utf8');
+            }
+            return $this->conn;
+        }
+        // EXECUTE QUESTION COMMAND
+        public function execute($sql) {
+            $this->result = $this->conn->query($sql);
+            return $this->result;
+        }
+        // METHOD TO GET DATA 
+        public function getData() {
+            if($this->result) {
+                $data = mysqli_fetch_array($this->result);
+            }
+            else {
+                $data = 0;
+            }
+            return $data;
+        }
+        //METHOD TO GET ALL DATA
+        public function getAllData {
+            if($this->getData()) {
+                while($tmpdata = $this->getData()) {
+                    $data[] = $tmpdata;
+                }
+            }
+            else {
+                $data = 0;
+            }
+            return $data;
+        }
+    }
+?>
