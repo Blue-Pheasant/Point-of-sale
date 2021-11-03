f18d79672598001b6ce348246bae76cc2290804b
Blue-Pheasant <BluePheasant@users.noreply.github.com>
2021-11-03 15:09:45 +0700

    Notthing

---
+++ exception/forbiddenException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace app\exception;
+
+
+use app\core\Application;
+
+class ForbiddenException extends \Exception
+{
+    protected $message = 'You don\'t have permission to access this page';
+    protected $code = 403;
+}
