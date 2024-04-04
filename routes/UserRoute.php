89823c973501e5beade8c16b3ccb6286ec2aa406
Blue-Pheasant <voquylong.2k1@gmail.com>
2024-04-04 21:35:21 +0700

    Implement routes by using Route in core pagekage

---
+++ routes/UserRoute.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace app\routes;
+
+use app\core\Route;
+use app\controllers\UserController;
+
+class UserRoute extends Route
+{
+    public function register()
+    {
+        // User routes
+        $this->get('/profile', [UserController::class, 'profile']);
+        $this->post('/profile', [UserController::class, 'updateProfile']);
+
+        // Admin routes
+        $this->get('/admin/users', [UserController::class, 'index']);
+        $this->get('/admin/users/delete', [UserController::class, 'delete']);
+        $this->get('/admin/users/edit', [UserController::class, 'update']);
+        $this->get('/admin/users/create', [UserController::class, 'create']);
+        $this->get('/admin/users/details', [UserController::class, 'details']);
+        $this->get('/admin/users/edit/password', [UserController::class, 'password']);
+        
+        $this->post('/admin/users/delete', [UserController::class, 'delete']);
+        $this->post('/admin/users/edit', [UserController::class, 'update']);
+        $this->post('/admin/users/create', [UserController::class, 'create']);
+        $this->post('/admin/users/details', [UserController::class, 'details']);
+        $this->post('/admin/users/edit/password', [UserController::class, 'password']);
+    }
+}
